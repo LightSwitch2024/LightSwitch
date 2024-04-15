c5c1b30eda8a1261fbbca9e2695882c66e6f6fd3
김동훈 <huni19541@gmail.com>
2024-04-15 13:20:25 +0900

    ADD java-server-sdk

---
+++ backend/sdk/src/test/java/com/lunitflag/sdk/SdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunitflag.sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
