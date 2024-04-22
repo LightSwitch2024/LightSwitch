9da32d7318a38ba66f8f62a2c4042769632c1790
박현우 <gogoadl@naver.com>
2024-04-22 13:58:03 +0900

    [CHORE] : swagger 의존성 추가
    http://localhost:8080/swagger-ui/index.html
    위의 경로로 swagger에 접속하면 됩니다.

---
+++ backend/core-service/src/main/kotlin/com/lightswitch/core/common/config/SwaggerConfig.kt
@@ -0,0 +1,21 @@
+package com.lightswitch.core.common.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+            .components(Components())
+            .info(apiInfo())
+
+    private fun apiInfo() = Info()
+            .title("Springdoc 테스트")
+            .description("Springdoc을 사용한 Swagger UI 테스트")
+            .version("1.0.0")
+}
---
+++ backend/core-service/src/main/kotlin/com/lightswitch/core/domain/log/controller/TestController.kt
@@ -0,0 +1,30 @@
+package com.lightswitch.core.domain.log.controller
+
+
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import io.swagger.v3.oas.annotations.Hidden
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.RequestParam
+
+
+@Tag(name = "예제 API", description = "Swagger 테스트용 API")
+@RestController
+@RequestMapping("/")
+class TestController {
+    @Operation(summary = "문자열 반복", description = "파라미터로 받은 문자열을 2번 반복합니다.")
+    @Parameter(name = "str", description = "2번 반복할 문자열")
+    @GetMapping("/returnStr")
+    fun returnStr(@RequestParam str: String) = "$str\n$str"
+
+    @GetMapping("/example")
+    fun example() = "예시 API"
+
+    @Hidden
+    @GetMapping("/ignore")
+    fun ignore() = "무시되는 API"
+}
