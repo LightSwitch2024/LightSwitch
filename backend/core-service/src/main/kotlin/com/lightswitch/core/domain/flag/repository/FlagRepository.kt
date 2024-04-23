d74f89b8f2657069b221b31e53744ae79a0bbe14
lion0077v <lion0077v@gmail.com>
2024-04-23 23:51:41 +0900

    [Feat] : 추가 Flag 간략 정보 전체 조회 테스트

--- backend/core-service/src/main/kotlin/com/lightswitch/core/domain/flag/repository/FlagRepository.kt
+++ backend/core-service/src/main/kotlin/com/lightswitch/core/domain/flag/repository/FlagRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FlagRepository : JpaRepository<Flag, Long>
+interface FlagRepository : JpaRepository<Flag, Long> {
+    fun countBy(): Long
+}
