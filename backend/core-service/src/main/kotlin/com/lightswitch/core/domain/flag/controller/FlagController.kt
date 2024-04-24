c4d11e83787d96e37f28741f6b684ad90a1a343f
홍윤기 <lion0077v@gmail.com>
2024-04-24 16:55:14 +0900

    [Feat] : 추가 Flag Controller 기능
    - 단건 조회 by flag id
    - 전체 조회
    - flag 생성

--- backend/core-service/src/main/kotlin/com/lightswitch/core/domain/flag/controller/FlagController.kt
+++ backend/core-service/src/main/kotlin/com/lightswitch/core/domain/flag/controller/FlagController.kt
@@ -4,10 +4,12 @@ import com.lightswitch.core.common.dto.BaseResponse
 import com.lightswitch.core.common.dto.success
 import com.lightswitch.core.domain.flag.dto.req.FlagRequestDto
 import com.lightswitch.core.domain.flag.dto.res.FlagResponseDto
+import com.lightswitch.core.domain.flag.dto.res.FlagSummaryDto
 import com.lightswitch.core.domain.flag.repository.entity.Flag
 import com.lightswitch.core.domain.flag.service.FlagService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -23,5 +25,15 @@ class FlagController(
     @PostMapping("")
     fun createFlag(@RequestBody flagRequestDto: FlagRequestDto): BaseResponse<FlagResponseDto> {
         return success(flagService.createFlag(flagRequestDto))
+    }
+
+    @GetMapping("")
+    fun getAllFlagsSummary(): BaseResponse<List<FlagSummaryDto>> {
+        return success(flagService.getAllFlag())
+    }
+
+    @GetMapping("/{flagId}")
+    fun getFlagDetail(@PathVariable flagId: Long): BaseResponse<FlagResponseDto> {
+        return success(flagService.getFlag(flagId))
     }
 }
