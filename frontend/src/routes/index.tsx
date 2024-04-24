fe1ff736ea7f8e1b820e4b3ab0e4578ee2c12e00
홍윤기 <lion0077v@gmail.com>
2024-04-24 16:09:58 +0900

    [Feat] : 추가 flag table 기능

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import FlagTable from '@/pages/main/index';
 import SignUp from '@/pages/signup/index';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/signUp" element={<SignUp />} />
+      <Route path="/table" element={<FlagTable />} />
     </Routes>
   </BrowserRouter>
 );
