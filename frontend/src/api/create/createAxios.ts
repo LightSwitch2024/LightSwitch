3e623f667f2a04807e05b2f9fa45a1aafad4706f
홍윤기 <lion0077v@gmail.com>
2024-04-24 23:08:33 +0900

    [Feat] : 추가 flag 생성 axios

---
+++ frontend/src/api/create/createAxios.ts
@@ -0,0 +1,39 @@
+import axios from '@api/axios';
+import { AxiosError, AxiosResponse } from 'axios';
+
+interface BaseResponse<T> {
+  code: number;
+  message: string;
+  data: T;
+}
+
+interface TagItem {
+  content: string;
+  colorHex: string;
+}
+
+interface FlagItem {
+  title: string;
+  tags: Array<TagItem>;
+  description: string;
+  type: string;
+  defaultValue: string;
+  defaultValuePortion: number;
+  defaultValueDescription: string;
+  variation: string;
+  variationPortion: number;
+  variationDescription: string;
+
+  userId: number;
+}
+
+export async function createFlag<T>(
+  data: FlagItem,
+  onSuccess: (data: T) => void,
+  onFail: (err: AxiosError) => void,
+): Promise<void> {
+  axios
+    .post<BaseResponse<T>>('/api/v1/flag', data)
+    .then((res: AxiosResponse<BaseResponse<T>>) => onSuccess(res.data.data))
+    .catch((err: AxiosError) => onFail(err));
+}
