ebf446602559c212352d5fb34d0a30c1950e87b6
홍윤기 <lion0077v@gmail.com>
2024-04-27 10:38:32 +0900

    [Style] : 추가 tab bar 제외 부분 스타일 적용

--- frontend/src/routes/indexStyle.tsx
+++ frontend/src/routes/indexStyle.tsx
@@ -2,5 +2,12 @@ import styles from 'styled-components';
 
 export const Content = styles.div`
     margin-left : 14.5rem;
+
+    display: flex;
     padding: 1.25rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.25rem;
+    flex: 1 0 0;
+    align-self: stretch;
 `;
