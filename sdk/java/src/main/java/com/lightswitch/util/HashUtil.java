db80fa8a64a01a8f70a63bc87214140c2d3d5ec8
이승헌 <317tmdgjs@naver.com>
2024-04-29 16:42:43 +0900

    [Refactor] : 수정 Java SDK User HashCode

--- sdk/java/src/main/java/com/lightswitch/util/HashUtil.java
+++ sdk/java/src/main/java/com/lightswitch/util/HashUtil.java
@@ -4,29 +4,29 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 public class HashUtil {
 
-	public static double getHashedPercentageForObjectIds(List<String> objectIds, int iterations) throws NoSuchAlgorithmException {
-		String toHash = String.join(",", objectIds);
-		toHash = String.valueOf(toHash).repeat(Math.max(0, iterations));
-
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		byte[] hashedBytes = md.digest(toHash.getBytes(StandardCharsets.UTF_8));
-		BigInteger no = new BigInteger(1, hashedBytes);
-
-		double value = (no.mod(BigInteger.valueOf(9999)).doubleValue() / 9998) * 100;
-
-		if (value == 100) {
-			return getHashedPercentageForObjectIds(objectIds, iterations + 1);
+	public static double getHashedPercentage(String contextId, int iterations) {
+		contextId = String.valueOf(contextId).repeat(Math.max(0, iterations));
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			byte[] hashedBytes = md.digest(contextId.getBytes(StandardCharsets.UTF_8));
+			BigInteger no = new BigInteger(1, hashedBytes);
+			double value = (no.mod(BigInteger.valueOf(9999)).doubleValue() / 9998) * 100;
+			if (value == 100) {
+				return getHashedPercentage(contextId, iterations + 1);
+			}
+			return value;
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
 		}
-
-		return value;
 	}
 
-	public static void main(String[] args) throws NoSuchAlgorithmException {
-		System.out.println(getHashedPercentageForObjectIds(List.of( "123"), 1));
-		System.out.println(getHashedPercentageForObjectIds(List.of( "121"), 1));
+	public static void main(String[] args) {
+		System.out.println(getHashedPercentage("123", 1));
+		System.out.println(getHashedPercentage("121", 1));
+
 	}
 }
