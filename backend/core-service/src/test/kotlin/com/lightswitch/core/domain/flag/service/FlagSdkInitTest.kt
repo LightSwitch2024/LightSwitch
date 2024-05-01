77af441df5f3dd89fb0d9d23a2dba700e6a18532
박현우 <gogoadl@naver.com>
2024-05-01 17:28:20 +0900

    [Fix] 제거 불필요 수정사항

--- backend/core-service/src/test/kotlin/com/lightswitch/core/domain/flag/service/FlagSdkInitTest.kt
+++ backend/core-service/src/test/kotlin/com/lightswitch/core/domain/flag/service/FlagSdkInitTest.kt
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @SpringBootTest
 class FlagSdkInitTest(
     @Autowired
--- backend/core-service/src/test/kotlin/com/lightswitch/core/domain/sse/service/SseServiceTest.kt
+++ backend/core-service/src/test/kotlin/com/lightswitch/core/domain/sse/service/SseServiceTest.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
 
 
+@Transactional
 @SpringBootTest
 class SseServiceTest(
     @Autowired
