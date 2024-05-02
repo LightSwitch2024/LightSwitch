ae31509acaa05548be3fe4cfc7c2089731d855e3
이승헌 <317tmdgjs@naver.com>
2024-05-02 21:06:52 +0900

    [Feat] : 추가 Java SDK HashCode 테스트

---
+++ sdk/java/src/test/java/com/lightswitch/util/HashUtilTest.java
@@ -0,0 +1,78 @@
+package com.lightswitch.util;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Random;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+class HashUtilTest {
+
+	@Test
+	void 결과는_0_100_사이의_값이다() {
+		double result1 = HashUtil.getHashedPercentage("123", 1);
+		double result2 = HashUtil.getHashedPercentage("121", 1);
+
+		assertThat(result1).isBetween(0.0, 100.0);
+		assertThat(result2).isBetween(0.0, 100.0);
+	}
+
+	@Test
+	void 유니코드_문자열로_균등한_백분율_분포를_테스트한다() {
+		Random random = new Random();
+		int lowerHalf = 0;
+		int upperHalf = 0;
+		int totalTests = 10000;
+
+		for (int i = 0; i < totalTests; i++) {
+			String randomUnicodeString = generateRandomUnicodeString(random);
+			double percentage = HashUtil.getHashedPercentage(randomUnicodeString, 1);
+
+			if (percentage < 50.0) {
+				lowerHalf++;
+			} else {
+				upperHalf++;
+			}
+		}
+
+		assertThat(lowerHalf).isGreaterThan(4000);
+		assertThat(upperHalf).isGreaterThan(4000);
+	}
+
+	private String generateRandomUnicodeString(Random random) {
+		return random.ints(48, 122)
+			.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+			.limit(10)
+			.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+			.toString();
+	}
+
+	@Test
+	void uuid로_균등한_백분율_분포를_테스트한다() {
+		int lowerHalf = 0;
+		int upperHalf = 0;
+		int totalTests = 10000;
+
+		for (int i = 0; i < totalTests; i++) {
+			String randomUUIDString = UUID.randomUUID().toString();
+			double percentage = HashUtil.getHashedPercentage(randomUUIDString, 1);
+
+			if (percentage < 50.0) {
+				lowerHalf++;
+			} else {
+				upperHalf++;
+			}
+		}
+
+		assertThat(lowerHalf).isGreaterThan(4000);
+		assertThat(upperHalf).isGreaterThan(4000);
+	}
+
+	@Test
+	void 결과는_정확히_100이_나오지_않는다() {
+		double result = HashUtil.getHashedPercentage("specificInput", 1);
+		assertThat(result).isNotEqualTo(100.0);
+	}
+
+}
