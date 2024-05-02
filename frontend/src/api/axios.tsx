3fe6b80f18c18f9494fc04828c946ff247497c8d
홍윤기 <lion0077v@gmail.com>
2024-05-02 15:27:17 +0900

    [Chore] : 수정 axios header 설정 변경

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: `${import.meta.env.VITE_SERVER_BASEURL}`,
+  timeout: 3000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  responseType: 'json',
 });
