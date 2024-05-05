8554fddca35d5b55cf4d9789ea0366857b6e31bc
박현우 <gogoadl@naver.com>
2024-05-05 21:03:16 +0900

    [Feat] : 수정 안드로이드 API 요청 구조

--- app/lightswitchapp/app/src/main/java/kr/lightswitch/ui/MainScreen.kt
+++ app/lightswitchapp/app/src/main/java/kr/lightswitch/ui/MainScreen.kt
@@ -2,10 +2,8 @@ package kr.lightswitch.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kr.lightswitch.model.response.Flag
@@ -19,18 +17,9 @@ fun MainScreen(mainViewModel: MainViewModel,
         when(uiState) {
             is UiState.Loading -> LoadingPage()
             is UiState.Error -> ErrorPage(error = uiState.error)
-            is UiState.Success -> FlagPage(uiState.data)
+            is UiState.Success<*> -> FlagPage(uiState.data as List<Flag>)
         }
     }
-
-//    val flags: List<Flag> by mainViewModel.flagState.collectAsStateWithLifecycle(initialValue = emptyList())
-//    Column (modifier = Modifier.fillMaxSize()){
-//        Text(text = flags.toString())
-//        Button(onClick = onBtnClick) {
-//            Text(text = "테스트 버튼 입니다. Flag Screen으로 이동합니다.")
-//        }
-//    }
-
 }
 
 @Composable
@@ -44,5 +33,5 @@ fun LoadingPage() {
 }
 @Composable
 fun FlagPage(data: List<Flag>) {
-    Text("Flag 데이터 표시")
+    Text(data.toString())
 }
--- app/lightswitchapp/app/src/main/java/kr/lightswitch/ui/MainViewModel.kt
+++ app/lightswitchapp/app/src/main/java/kr/lightswitch/ui/MainViewModel.kt
@@ -1,6 +1,7 @@
 package kr.lightswitch.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
@@ -11,6 +12,7 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.launch
 import kr.lightswitch.model.response.Flag
 import kr.lightswitch.network.LightSwitchRepository
 import timber.log.Timber
@@ -25,22 +27,22 @@ class MainViewModel @Inject constructor(
     val uiState = _uiState.asStateFlow()
 
     init {
-        lightSwitchRepository.getExample(
-            onStart = {
-                Timber.d("onStart getExample")
-                _uiState.value = UiState.Loading
-            },
-            onComplete = {
-                Timber.d("onComplete getExample")
-            },
-            onError = {
-                    error ->
-                Timber.d("error occured : $error")
-                _uiState.value = UiState.Error(error)
+        viewModelScope.launch {
+            lightSwitchRepository.getExample(
+                onStart = {
+                    Timber.d("onStart getExample")
+                    _uiState.value = UiState.Loading
+                },
+                onComplete = {
+                    Timber.d("onComplete getExample")
+                },
+                onError = { error ->
+                    Timber.d("error occured : $error")
+                    _uiState.value = UiState.Error(error)
+                }
+            ).collectLatest {response ->
+                _uiState.value = UiState.Success(response.data)
             }
-        ).mapNotNull {
-                _uiState.value = UiState.Success(it.data)
-                it.data
-            }
+        }
     }
 }
