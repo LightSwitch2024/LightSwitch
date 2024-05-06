0a38cc3a4d6bc3420e687c003489851ead6977c4
박현우 <gogoadl@naver.com>
2024-05-06 15:54:34 +0900

    [Fix] : 수정 라우팅 오류

--- app/lightswitchapp/app/src/main/java/kr/lightswitch/ui/login/LoginScreen.kt
+++ app/lightswitchapp/app/src/main/java/kr/lightswitch/ui/login/LoginScreen.kt
@@ -1,40 +1,26 @@
 package kr.lightswitch.ui.login
 
-import android.widget.Toast
-import androidx.annotation.ColorRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.autoSaver
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -42,6 +28,7 @@ import androidx.navigation.NavController
 import kr.lightswitch.LightSwitchApplication
 import kr.lightswitch.R
 import kr.lightswitch.ui.NavScreen
+import timber.log.Timber
 
 @Composable
 fun LoginScreen(
@@ -56,6 +43,7 @@ fun LoginScreen(
         LightSwitchApplication.getInstance().getDataStore().isLogin.collect {
             if(it) {
                 println("로그인 상태 확인됨")
+                navController.popBackStack()
                 navController.navigate(NavScreen.Flags.route)
             } else {
                 println("로그인 상태 확인되지 않음")
@@ -127,8 +115,10 @@ fun LoginScreen(
         )
 
         // 로그인 응답 데이터가 있으면 표시
-        loginResponse?.let {
-            navController.navigate(NavScreen.Flags.route)
-        }
+//        loginResponse?.let {
+//            Timber.d("flag로 라우팅")
+//            navController.popBackStack()
+//            navController.navigate(NavScreen.Flags.route)
+//        }
     }
 }
