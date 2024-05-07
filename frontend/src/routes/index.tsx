35114968806af177d004f64f3765db1b373392ff
이신애 <gosin77@snu.ac.kr>
2024-05-07 13:29:37 +0900

    [Refactor]:수정 회원가입창 모달로 변경

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,26 +1,15 @@
 import Tab from '@components/tab/index';
 import CreateFlag from '@pages/create/index';
 import * as S from '@routes/indexStyle';
-import { useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
+import SignUp from '@/components/signup/index';
 import FlagDetail from '@/pages/flag/index';
 import Main from '@/pages/main/index';
 import UserDetail from '@/pages/mypage/index';
-import SignUp from '@/pages/signup/index';
 import LogIn from '@/pages/user/login';
 
 import ProtectedRoute from './protectedRoute';
-
-// const LocationObserver = () => {
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     console.log('Route changed to:', location.pathname);
-//   }, [location]);
-
-//   return null; // 이 컴포넌트는 UI를 렌더링하지 않습니다.
-// };
 
 const Router = () => {
   const location = useLocation();
@@ -35,8 +24,6 @@ const Router = () => {
           <Route path="/" element={<ProtectedRoute />}>
             <Route path="/" element={<Main />} />
           </Route>
-          {/* <Route path="/" element={<Main />} /> */}
-          <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/create" element={<CreateFlag />} />
           <Route path="/flag/:flagId" element={<FlagDetail />} />
