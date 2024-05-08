a90386e15a36218d765b77f0323f5e147b07955b
홍윤기 <lion0077v@gmail.com>
2024-05-08 17:21:50 +0900

    [Fix] : 수정 getFlag 쿼리 오류 수정
    예외상황 추가하여 jpql 수정하였습니다.

--- backend/core-service/src/main/kotlin/com/lightswitch/core/domain/flag/repository/FlagRepository.kt
+++ backend/core-service/src/main/kotlin/com/lightswitch/core/domain/flag/repository/FlagRepository.kt
@@ -3,6 +3,7 @@ package com.lightswitch.core.domain.flag.repository
 import com.lightswitch.core.domain.flag.repository.entity.Flag
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -18,7 +19,14 @@ interface FlagRepository : JpaRepository<Flag, Long> {
     @Query("SELECT f FROM flag f JOIN FETCH f.keywords k WHERE k.deletedAt IS NULL AND f.flagId = :flagId")
     fun findFlagWithActiveKeywords(flagId: Long): Flag?
 
-    @Query("SELECT f FROM flag f WHERE NOT EXISTS (SELECT k FROM f.keywords k WHERE k.deletedAt IS NOT NULL) AND f.flagId = :flagId")
-    fun findFlagsWithNoDeletedKeywords(flagId: Long): Flag?
+//    @Query("SELECT f FROM flag f WHERE EXISTS (SELECT k FROM f.keywords k WHERE k.deletedAt IS NULL) AND f.flagId = :flagId")
+//    fun findFlagsWithNoDeletedKeywords(@Param("flagId") flagId: Long): Flag?
+
+    @Query(
+        "SELECT f FROM flag f WHERE f.flagId = :flagId AND " +
+                "(EXISTS (SELECT k FROM f.keywords k WHERE k.flag = f AND k.deletedAt IS NULL) OR " +
+                "NOT EXISTS (SELECT k FROM f.keywords k WHERE k.flag = f))"
+    )
+    fun findFlagsWithNoDeletedKeywords(@Param ("flagId") flagId: Long): Flag?
 
 }
