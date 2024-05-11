2aaabec45f3102ddd75e91d6c6ed8ba34c6e11c9
이승헌 <317tmdgjs@naver.com>
2024-05-11 15:15:43 +0900

    [Refactor] : 변경 Java SDK LS응답 관리코드 분리

--- sdk/java/src/main/java/com/lightswitch/impl/LightSwitchImpl.java
+++ sdk/java/src/main/java/com/lightswitch/impl/LightSwitchImpl.java
@@ -1,15 +1,8 @@
 package com.lightswitch.impl;
 
-import static java.nio.charset.StandardCharsets.*;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.util.List;
-import java.util.Objects;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -25,13 +18,13 @@ import com.lightswitch.domain.dto.UserKeyResponse;
 import com.lightswitch.exception.LSFlagRuntimeException;
 import com.lightswitch.exception.LSLSFlagNotFoundException;
 import com.lightswitch.exception.LSServerException;
-import com.lightswitch.util.HttpConnector;
+import com.lightswitch.util.LSConnector;
 
-public class LightSwitchImpl implements LightSwitch {
+public class LightSwitchImpl implements LightSwitch, SseCallback {
 
 	private Thread thread;
-	private String hostUrl;
 	private String userKey;
+	private LSConnector connector;
 
 	private LightSwitchImpl() {
 	}
@@ -50,120 +43,46 @@ public class LightSwitchImpl implements LightSwitch {
 			return;
 		}
 		destroy();
+		connector = new LSConnector(serverUrl);
 
-		hostUrl = serverUrl;
+		HttpURLConnection initConn = connector.setup("sdk/init", "POST", false);
+		connector.sendData(initConn, new Config(sdkKey));
+		getAllFlags(initConn);
 
-		HttpURLConnection initConnection = setupConnection("sdk/init", "POST", false);
-		sendData(initConnection, new Config(sdkKey));
-		getAllFlags(initConnection);
+		HttpURLConnection subscribeConn = connector.setup("sse/subscribe", "POST", false);
+		connector.sendData(subscribeConn, new Config(sdkKey));
+		userKey = getUserKey(subscribeConn);
 
-		HttpURLConnection subscribeConnection = setupConnection("sse/subscribe", "POST", false);
-		sendData(subscribeConnection, new Config(sdkKey));
-		userKey = getUserKey(subscribeConnection);
-
-		HttpURLConnection connection = setupConnection("sse/subscribe/" + userKey, "GET", true);
-		connectToSse(connection);
+		HttpURLConnection sseConn = connector.setup("sse/subscribe/" + userKey, "GET", true);
+		connectToSse(sseConn);
 	}
 
-	private HttpURLConnection setupConnection(String endpoint, String method, boolean isSSE) throws
-		LSServerException {
-		return new HttpConnector().getConnect(hostUrl, endpoint, method, 0, isSSE);
-	}
-
-	private void getAllFlags(HttpURLConnection initConnection) throws LSFlagRuntimeException {
+	private void getAllFlags(HttpURLConnection initConn) throws LSFlagRuntimeException {
 		Type responseType = new TypeToken<BaseResponse<List<FlagResponse>>>() {
 		}.getType();
-		List<FlagResponse> flags = processConnectionResponse(initConnection, responseType);
+		List<FlagResponse> flags = connector.getResponse(initConn, responseType);
 		Flags.addAllFlags(flags);
 	}
 
-	private String getUserKey(HttpURLConnection subscribeConnection) throws LSFlagRuntimeException {
+	private String getUserKey(HttpURLConnection subscribeConn) throws LSFlagRuntimeException {
 		Type responseType = new TypeToken<BaseResponse<UserKeyResponse>>() {
 		}.getType();
-		UserKeyResponse userKeyResponse = processConnectionResponse(subscribeConnection, responseType);
+		UserKeyResponse userKeyResponse = connector.getResponse(subscribeConn, responseType);
 		return userKeyResponse.getUserKey();
 	}
 
-	private <T> T processConnectionResponse(HttpURLConnection connection, Type responseType) throws
-		LSFlagRuntimeException {
-		BaseResponse<T> response = handleResponse(connection, responseType);
-		if (response.getCode() != HttpURLConnection.HTTP_OK) {
-			throw new LSServerException();
-		}
-		return response.getData();
-	}
-
-	private <T> T handleResponse(HttpURLConnection connection, Type responseType) throws LSServerException {
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), UTF_8))) {
-			String response = parsingResponse(reader);
-			return getJsonObject(response, responseType);
-		} catch (IOException e) {
-			throw new LSServerException();
-		}
-	}
-
-	private <T> T getJsonObject(String jsonData, Type responseType) {
-		return new Gson().fromJson(jsonData, responseType);
-	}
-
-	private int sendData(HttpURLConnection connection, Object body) throws
-		LSServerException {
-		try (OutputStream os = connection.getOutputStream()) {
-			String json = new Gson().toJson(body);
-			byte[] input = json.getBytes(UTF_8);
-			os.write(input, 0, input.length);
-			return connection.getResponseCode();
-		} catch (IOException e) {
-			throw new LSServerException();
-		}
-	}
-
-	private void connectToSse(HttpURLConnection connection) throws LSServerException {
-		Runnable task = () -> {
-			try (BufferedReader reader = new BufferedReader(
-				new InputStreamReader(connection.getInputStream(), UTF_8))) {
-
-				while (!Thread.interrupted()) {
-					String sseResponse = parsingResponse(reader);
-					processJsonData(sseResponse);
-				}
-			} catch (IOException io) {
-				throw new LSServerException();
-			}
-		};
-
+	private void connectToSse(HttpURLConnection sseConn) throws LSServerException {
+		Runnable task = connector.createSseRunnable(sseConn, this); //onSseReceived
 		thread = new Thread(task);
 		thread.start();
 	}
 
-	private void processJsonData(String jsonData) {
+	@Override
+	public void onSseReceived(String jsonData) {
 		if (jsonData.isEmpty()) {
 			return;
 		}
-		SseResponse sseResponse = getJsonObject(jsonData, SseResponse.class);
-		Flags.event(sseResponse);
-	}
-
-	private String parsingResponse(BufferedReader reader) throws IOException {
-		StringBuilder response = new StringBuilder();
-		String inputLine;
-		while (Objects.nonNull(inputLine = reader.readLine())) {
-			if (inputLine.startsWith("event:")) {
-				if (inputLine.substring(6).contains("disconnect")) {
-					break;
-				}
-			} else if (inputLine.startsWith("data:")) {
-				inputLine = inputLine.substring(5);
-				if (inputLine.contains("SSE connected")) {
-					break;
-				}
-				response.append(inputLine);
-			} else {
-				response.append(inputLine.trim());
-				break;
-			}
-		}
-		return response.toString();
+		Flags.event(new Gson().fromJson(jsonData, SseResponse.class));
 	}
 
 	@Override
@@ -171,10 +90,8 @@ public class LightSwitchImpl implements LightSwitch {
 		if (thread != null) {
 			thread.interrupt();
 			thread = null;
-			HttpURLConnection disconnectConnection = setupConnection("sse/disconnect", "DELETE", false);
-			if (sendData(disconnectConnection, new UserKeyRequest(userKey)) != HttpURLConnection.HTTP_OK) {
-				throw new LSServerException();
-			}
+			HttpURLConnection disconnectConnection = connector.setup("sse/disconnect", "DELETE", false);
+			connector.sendData(disconnectConnection, new UserKeyRequest(userKey));
 		}
 		Flags.clear();
 	}
