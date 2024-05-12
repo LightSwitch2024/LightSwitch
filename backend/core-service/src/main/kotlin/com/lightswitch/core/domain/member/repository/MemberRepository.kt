88213614a98571e0cf91249f96408f35a9817494
이신애 <zigzaag@naver.com>
2024-05-12 19:08:34 +0900

    [Refactor]:수정 오빌드 memberRepository

--- backend/core-service/src/main/kotlin/com/lightswitch/core/domain/member/repository/MemberRepository.kt
+++ backend/core-service/src/main/kotlin/com/lightswitch/core/domain/member/repository/MemberRepository.kt
@@ -9,7 +9,5 @@ import org.springframework.stereotype.Repository
 interface MemberRepository : JpaRepository<Member, Long> {
     fun findByEmailAndDeletedAtIsNull(email: String): Member?
 
-    fun findByIdAndDeletedAtIsNull(memberId: Long): Member?
-
     fun findAllAByDeletedAtIsNull(): List<Member>
 }
