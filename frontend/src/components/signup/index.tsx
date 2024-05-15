637f9cf683196c23c34bcbea6ac29680906cd14e
박현우 <gogoadl@naver.com>
2024-05-15 20:20:47 +0900

    [Fix] : 수정 JSON 파라미터 오류

--- frontend/src/components/signup/index.tsx
+++ frontend/src/components/signup/index.tsx
@@ -23,6 +23,7 @@ type SignUpData = {
   firstName: string;
   lastName: string;
   telNumber: string;
+  orgName: string;
   email: string;
   password: string;
   authCode: string;
@@ -166,6 +167,7 @@ const SignUp: React.FC<Props> = ({ isSignUpModal, onClose }) => {
     const signUpData: SignUpData = {
       firstName: firstName,
       lastName: lastName,
+      orgName: orgName,
       telNumber: telNumber,
       email: email,
       password: password,
