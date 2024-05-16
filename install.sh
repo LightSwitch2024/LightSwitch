d227e73a4eba300dcf6de837afe623b3611f9c1a
김동훈 <huni19541@gmail.com>
2024-05-16 17:14:12 +0900

    [Feat] : 작업 오픈소스
    -
    
    [Resolves] : feature-infra/opensource

--- install.sh
+++ install.sh
@@ -1,23 +1,26 @@
 service postgresql start
 
-git clone
+sudo -u postgres psql -f /home/conf/database-setup.sql
 
-'' backend build
-cd /backend/core-service
-#dev-yml 수정
+domain="$DOMAIN"
+echo "도메인: $domain"
+
+git clone https://github.com/LightSwitch-S202/LightSwitch.git
+cd /LightSwitch
+
+cd ./backend/core-service
 ./graldew clean assemble
 cd ./build/libs
-
+jar_file=$(ls *.jar)
+echo "실행할 .jar 파일: $jar_file"
+nohup java -jar $jar_file -DSpring.cors.domain=$domain &
+cd /LightSwitch
 
 '' frontend build
-.env 수정
+cd ./frontend
+echo VITE_SERVER_BASEURL=$domain > .env
 yarn install
 yarn build
-
-cp
+cp -r /LightSwitch/frontend/dist/* /var/www/html
 
 service nginx start
-sudo -u postgres psql -f /home/conf/database-setup.sql
-#psql -U postgres -f /home/conf/database-setup.sql
-
-java -jar -Duser.timezone=Asia/Seoul /home/conf/app.jar
